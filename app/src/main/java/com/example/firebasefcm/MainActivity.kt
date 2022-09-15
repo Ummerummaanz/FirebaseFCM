3757f3ff0966c989f1a8b057ff363d63d111d31b
rumaan <rummaanumme@gmail.com>
2022-09-15 14:40:29 +0530

    FireBase storage

--- app/src/main/java/com/example/firebasefcm/MainActivity.kt
+++ app/src/main/java/com/example/firebasefcm/MainActivity.kt
@@ -4,15 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
+    lateinit var etName: EditText
+    lateinit var tvMain: TextView
+    lateinit var etPaswd: EditText
+    val db = Firebase.firestore
+
     var TAG = MainActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etName = findViewById(R.id.etName)
+        tvMain = findViewById(R.id.tvMain)
+        etPaswd = findViewById(R.id.etPassword)
+
+        Log.i(TAG,"im in oncreate")
     }
 
     fun getRegnTokenFCM(view: View) {
@@ -31,5 +46,40 @@ class MainActivity : AppCompatActivity() {
                 Log.d(TAG, token)
                 Toast.makeText(this, token, Toast.LENGTH_SHORT).show()
             })
+    }
+
+    fun putFirestore(view: View) {
+        // Create a new user with a first, middle, and last name
+        val user = hashMapOf(
+            "first" to etName.text.toString(),
+            "last" to etPaswd.text.toString(),
+        )
+
+// Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
+
+    }
+
+
+    fun getFireStore(view: View) {
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
+
     }
 }
